e7f1d203f1b01cc39b96e2251423f662d0565c62
misraX <maysragamal@gmail.com>
2022-02-05 17:25:19 +0200

    Adding device and levels model admin

--- apps/level/admin.py
+++ apps/level/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from apps.level.models import UserGlucoseLevel
+
+
+@admin.register(UserGlucoseLevel)
+class UserGlucoseLevelModelAdmin(admin.ModelAdmin):
+    pass
