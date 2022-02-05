4ffc9be529c7ab16d796feabc8091f27a4ee5749
misraX <maysragamal@gmail.com>
2022-02-05 17:36:18 +0200

    Adding swagger Limiting views to update, list, retrieve, create

--- apps/level/api/views.py
+++ apps/level/api/views.py
@@ -1,11 +1,16 @@
-from rest_framework import viewsets
+from rest_framework import mixins
+from rest_framework.viewsets import GenericViewSet
 
 from apps.level.api.filters import UserGlucoseLeveFilterSet
 from apps.level.api.serializers import UserGlucoseLevelSerializer
 from apps.level.models import UserGlucoseLevel
 
 
-class LevelModelViewSet(viewsets.ModelViewSet):
+class LevelModelViewSet(mixins.CreateModelMixin,
+                        mixins.RetrieveModelMixin,
+                        mixins.UpdateModelMixin,
+                        mixins.ListModelMixin,
+                        GenericViewSet):
     """
     A simple ViewSet for viewing and editing accounts.
     """
