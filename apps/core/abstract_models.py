1a09da6cd8c2a83336ba4ff5db96f3c6cc41fddf
misraX <maysragamal@gmail.com>
2022-02-05 15:22:03 +0200

    Adding core with abstract_models

---
+++ apps/core/abstract_models.py
@@ -0,0 +1,13 @@
+import uuid as uuid
+
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+from model_utils.models import TimeStampedModel
+
+
+class AbstractBaseModel(TimeStampedModel):
+    uuid = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
+    meta = models.JSONField(_("meta"), null=True)
+
+    class Meta:
+        abstract = True
